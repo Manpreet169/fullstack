81b4d3ab01c56e254d95a6363a24ba17d8753b1f
Manpreet Singh <97599275+Manpreet169@users.noreply.github.com>
2024-04-02 15:09:00 +0200

    Add files via upload

---
+++ SETTIMANA2/giorno1/D1.js
@@ -0,0 +1,109 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.*/
+
+/*
+ Stringa (String): Una stringa è una sequenza di caratteri, come lettere, numeri o simboli, racchiusa tra virgolette singole o doppie. Ad esempio, "ciao" è una stringa. Le stringhe sono utilizzate per rappresentare testo e possono essere manipolate e combinate.
+
+1. Numero (Number): Questo tipo di dato rappresenta sia numeri interi che numeri con virgola mobile. Può essere utilizzato per eseguire operazioni matematiche come l'addizione, la sottrazione, la moltiplicazione e la divisione.
+
+2. Booleano (Boolean): Un booleano può essere vero o falso, e viene utilizzato per rappresentare valori di verità. Ad esempio, true (vero) e false (falso) sono valori booleani. Sono utilizzati spesso nelle istruzioni condizionali e nei test di controllo del flusso.
+
+3. Oggetto (Object): Gli oggetti sono collezioni di coppie chiave-valore, dove ogni chiave è una stringa unica e ogni valore può essere di qualsiasi tipo di dato, incluso un altro oggetto. Gli oggetti sono utilizzati per rappresentare entità complesse e strutturate, e permettono di organizzare e manipolare dati in modo efficac
+
+4. Null: Il valore null rappresenta l'assenza intenzionale di un valore. Può essere assegnato a variabili o proprietà per indicare che non hanno un valore valido.
+
+5. Indefinito (Undefined): Il valore undefined indica che una variabile è stata dichiarata ma non è stata inizializzata con un valore. Viene restituito quando si tenta di accedere a una variabile che non è stata definita o a una proprietà di un oggetto che non esiste. 
+*/
+
+
+
+
+
+
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.*/
+/* var myName = "Manpreet"; */
+
+
+
+
+
+
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.*/
+
+/* 
+var risultato = 12 + 20;
+console.log(risultato);
+*/
+
+
+
+
+
+
+
+/* ESERCIZIO 4 Crea una variable di nome "x" e assegna ad essa il numero 12.*/
+
+/* 
+var x = 12;
+*/
+
+
+
+
+
+
+
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/*
+const MANPREET = "Manpreet";
+
+var MANPREET = "giacomo";
+
+console.log(myName); // Stampa "Cognome"
+ */
+
+
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/*
+ var x = 12;
+    y = x - 4;
+ */
+
+
+
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+
+*/
+
+/* 
+let name1 = "john";
+let name2 = "John"; 
+let sonoDiverse = name1 !== name2;
+*/
